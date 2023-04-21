dff1e6aec2cfe5e9b9080188ee6243a3f5820ecc
Brian McIntosh <brian@mixinteractive.com>
2023-04-21 08:20:04 -0400

    Initial Commit

---
+++ EnvironmentDynamicType/EnvironmentDynamicTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentDynamicTypeApp.swift
+//  EnvironmentDynamicType
+//
+//  Created by Brian McIntosh on 4/21/23.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentDynamicTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b4bf640254eaef677fbf3c26909385e5ca320d3e
Brian McIntosh <brian@mixinteractive.com>
2023-04-21 08:39:32 -0400

    Added Avatar, HStack, and Environment Property

--- EnvironmentDynamicType/ContentView.swift
+++ EnvironmentDynamicType/ContentView.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.dynamicTypeSize) var dynamicTypeSize
+    //enum: accessibility 1, 2, 3, etc
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        HStack {
+            Image("Brian")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(Circle()) //Or, .mask(Circle())
+            
+            Text("Brian McIntosh")
+                .bold()
+                .font(.title)
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
     }
 }

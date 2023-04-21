c5d16effb1aaef13cc0f35c46fe320b74bf51481
Brian McIntosh <brian@mixinteractive.com>
2023-04-21 08:53:51 -0400

    Update UI based on Dynamic Type Sizes

--- EnvironmentDynamicType/ContentView.swift
+++ EnvironmentDynamicType/ContentView.swift
@@ -11,18 +11,34 @@ struct ContentView: View {
     
     @Environment(\.dynamicTypeSize) var dynamicTypeSize
     //enum: accessibility 1, 2, 3, etc
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
-
-        HStack {
-            Image("Brian")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .clipShape(Circle()) //Or, .mask(Circle())
-            
-            Text("Brian McIntosh")
-                .bold()
-                .font(.title)
+        if dynamicTypeSize >= .accessibility2 {
+            VStack {
+                Image("Brian")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle()) //Or, .mask(Circle())
+                
+                Text("Brian McIntosh")
+                    .bold()
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(colorScheme == .dark ? .red : .green)
+            }
+        }else{
+            HStack {
+                Image("Brian")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle()) //Or, .mask(Circle())
+                
+                Text("Brian McIntosh")
+                    .bold()
+                    .font(.title)
+                    .foregroundColor(colorScheme == .dark ? .red : .green)
+            }
         }
     }
 }
@@ -30,6 +46,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+            .environment(\.dynamicTypeSize, .accessibility1)
+        //same thing:
+        //.environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
     }
 }
